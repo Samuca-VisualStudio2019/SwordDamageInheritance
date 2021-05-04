2b5698f2a72d2202da559b9cf25e94f4ebda0d7e
Samuca-VisualStudio2019 <samucateclas@gmail.com>
2021-05-04 17:59:45 -0300

    Project Started

---
+++ SwordDamageInheritance/SwordDamageInheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SwordDamageInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
