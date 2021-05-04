05ab8c765334f9e633c01d1c8880874d296fa492
Samuca-VisualStudio2019 <samucateclas@gmail.com>
2021-05-04 18:05:29 -0300

    Add ArrowDamage Steps 1, 2 and 3

---
+++ SwordDamageInheritance/SwordDamageInheritance/ArrowDamage.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwordDamageInheritance
+{
+    class ArrowDamage
+    {
+        private const decimal BASE_MULTIPLIER = 0.35M;
+        private const decimal MAGIC_MULTIPLIER = 2.5M;
+        private const decimal FLAME_DAMAGE = 2.5M;
+
+        /// <summary>
+        /// Contains the calculate Damage
+        /// </summary>
+        public int Damage { get; private set; }
+
+        private int roll;
+
+        /// <summary>
+        /// Sets or Gets the 3d6 roll.
+        /// </summary>
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+        private bool magic;
+
+        /// <summary>
+        /// True if the sword is magic, false otherwise.
+        /// </summary>
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+
+        private bool flaming;
+
+        /// <summary>
+        /// True if the sword is flaming, false otherwise.
+        /// </summary>
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+
+        /// <summary>
+        /// Calculates the damage based on the current properties
+        /// </summary>
+
+        private void CalculateDamage()
+        {
+            decimal magicMultiplier = 1M;
+            if (magic) magicMultiplier = 1.75M;
+
+            Damage = BASE_DAMAGE;
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+            if (flaming) Damage += FLAME_DAMAGE;
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="startingroll">Starting 3d6 roll</param>
+        public ArrowDamage(int startingroll)
+        {
+            roll = startingroll;
+            CalculateDamage();
+        }
+    }
+}
---
+++ SwordDamageInheritance/SwordDamageInheritance/SwordDamage.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace VisualCalculateDamage
+{
+    class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        /// <summary>
+        /// Contains the calculate Damage
+        /// </summary>
+        public int Damage { get; private set; }
+
+        private int roll;
+
+        /// <summary>
+        /// Sets or Gets the 3d6 roll.
+        /// </summary>
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+        private bool magic;
+
+        /// <summary>
+        /// True if the sword is magic, false otherwise.
+        /// </summary>
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+
+        private bool flaming;
+
+        /// <summary>
+        /// True if the sword is flaming, false otherwise.
+        /// </summary>
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+
+        /// <summary>
+        /// Calculates the damage based on the current properties
+        /// </summary>
+
+        private void CalculateDamage()
+        {
+            decimal magicMultiplier = 1M;
+            if (magic) magicMultiplier = 1.75M;
+
+            Damage = BASE_DAMAGE;
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE;
+            if (flaming) Damage += FLAME_DAMAGE;
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="startingroll">Starting 3d6 roll</param>
+        public SwordDamage(int startingroll)
+        {
+            roll = startingroll;
+            CalculateDamage();
+        }
+
+    }
+}
